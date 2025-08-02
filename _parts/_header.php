6b209c019cddc14f8847e3b343908489ba5d6f62
JoelMbn <joelzinhoneto8@gmail.com>
2025-08-02 15:06:24 -0400

    Login com CSS

--- _parts/_header.php
+++ _parts/_header.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="base.css">
-    <title>Document</title>
-</head>
-<body>
-    <header class="head">
-        <img src="placeholder.png" alt="Profile" class="pfp">
+<img src="placeholder.png" alt="Profile" class="pfp">
         <nav class="navbar navbar-expand-lg custom-navbar">
             <div class="container-fluid">
               <a class="navbar-brand" href="#"></a>
@@ -27,15 +16,9 @@
                     <a class="nav-link active" aria-current="page" href="#">Contatos</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="login.php">Login</a>
                   </li>
                 </ul>
               </div>
             </div>
           </nav>
-          
-    </header>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
